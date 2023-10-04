ff7e6ef25f6b883afd3cc8098ee5bac2c215fecd
libo <156884481@qq.com>
2023-10-04 13:38:56 +0800

    程序框架

---
+++ springboot-hateoas/src/main/kotlin/cn/liveup/examples/springboothateoas/SpringbootHateoasApplication.kt
@@ -0,0 +1,11 @@
+package cn.liveup.examples.springboothateoas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootHateoasApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootHateoasApplication>(*args)
+}
---
+++ springboot-hateoas/src/test/kotlin/cn/liveup/examples/springboothateoas/SpringbootHateoasApplicationTests.kt
@@ -0,0 +1,13 @@
+package cn.liveup.examples.springboothateoas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootHateoasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
